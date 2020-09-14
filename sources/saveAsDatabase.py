2f1b19f5a84d418668c266a9d86a4b837408ca17
XYTriste <1106693417@qq.com>
2020-09-14 23:10:16 +0800

    创建了数据库的创建函数,还需要完善。

--- sources/saveAsDatabase.py
+++ sources/saveAsDatabase.py
@@ -2,17 +2,24 @@ import pymysql
 
 import json
 
-# heroDB = pymysql.connect(host='localhost', user='root', password='sa', port=3306)
-# cursor = heroDB.cursor()
+
+heroDB = pymysql.connect(host='localhost', user='root', password='sa', port=3306, db='heroinformation')
+cursor = heroDB.cursor()
 
 def main():
     with open("..\\resources\\heroList.json", "r", encoding="utf8") as f:
         heroJsonStr = json.loads(f.read())
-        if not 'hero' in heroJsonStr.keys():
+        if 'hero' not in heroJsonStr.keys():
             return
+        heroList = heroJsonStr['hero']
+        attrs = list(heroList[0].keys())
+        create_database()
+
 
 def create_database():
-    pass
+    sql = 'CREATE DATABASE IF NOT EXISTS HeroInformation DEFAULT CHARSET utf8'
+    cursor.execute(sql)
+
 
 if __name__ == '__main__':
     main()
