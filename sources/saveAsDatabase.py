e203b070b651f72671c1911e62021e5ff3167bad
Triste <1106693417@qq.com>
2020-09-19 15:37:49 +0800

    将爬虫操作保存为一个用于调用的方法,每次从文件读取之前首先调用爬虫对数据进行更新.增加了三个用于保存版本号、文件名、文件时间的数据表.以便于在更新数据库中的数据前进行判断是否需要更新.优化了代码逻辑.修复了若干bug.

--- sources/saveAsDatabase.py
+++ sources/saveAsDatabase.py
@@ -4,7 +4,9 @@ import json
 
 import re
 
-heroDB = pymysql.connect(host='localhost', user='root', password='sa', port=3306, db='heroinformation')  # 链接数据库
+import Spider
+
+heroDB = pymysql.connect(host='localhost', user='root', password='sa', port=3306)  # 链接数据库
 cursor = heroDB.cursor()  # 获取数据库的游标
 
 
@@ -13,14 +15,18 @@ def create_database():  # 创建数据库的函数
     cursor.execute(sql)
 
 
-def create_table(keys):  # 创建表的函数
+def create_table(keys, table='hero'):  # 创建表的函数
 
-    # 此处说明:由于暂时的水平有限,所以只能把所有字段都设置为字符型,长度固定,等以后技术提升了再想办法更新.
-    sql = "CREATE TABLE IF NOT EXISTS hero("
-    sql += " VARCHAR(60) NOT NULL,".join(keys)
-    sql += " VARCHAR(255) NOT NULL, PRIMARY KEY (heroId))"
-    cursor.execute(sql)
-    modify_table_type()
+    if table == 'hero':
+        # 此处说明:由于暂时的水平有限,所以只能把所有字段都设置为字符型,长度固定,等以后技术提升了再想办法更新.
+        sql = "CREATE TABLE IF NOT EXISTS hero("
+        sql += " VARCHAR(60) NOT NULL,".join(keys)
+        sql += " VARCHAR(255) NOT NULL, PRIMARY KEY (heroId))"
+        cursor.execute(sql)
+        modify_table_type()
+    else:
+        sql = 'CREATE TABLE IF NOT EXISTS ' + table + "(" + table + " VARCHAR(30) NOT NULL)"
+        cursor.execute(sql)
 
 
 def modify_table_type():
@@ -32,6 +38,7 @@ def modify_table_type():
 
 def insert_data(table, heroList, keys):
     heroListLen = len(heroList)
+    print("heroListLen", heroListLen)
     for i in range(heroListLen):
         attrs = ",".join(heroList[i].keys())
         values = ",".join(
@@ -45,8 +52,22 @@ def insert_data(table, heroList, keys):
                 print('Successful')
                 heroDB.commit()
         except Exception as e:
-            # print(e)
+            print(e)
             print('Failed')
+            heroDB.rollback()
+
+
+def insert_other_data(heroJsonStr, keys):
+    for key in keys:
+        data = heroJsonStr[key]
+        table = key
+        sql = "Insert into {table}({keys}) VALUES (\'{values}\')".format(table=table, keys=key, values=data)
+        try:
+            if cursor.execute(sql):
+                print('OtherData Successful')
+                heroDB.commit()
+        except Exception as e:
+            print('OtherData Failed')
             heroDB.rollback()
 
 
@@ -57,7 +78,7 @@ def table_isExists():
     table_list = re.findall('(\'.*?\')', str(tables))
     table_list = [re.sub("'", '', each) for each in table_list]
 
-    #print(table_list)
+    # print(table_list)
 
     if 'hero' in table_list:
         return True
@@ -66,20 +87,30 @@ def table_isExists():
 
 
 def main():
+    Spider.getData()
     with open("..\\resources\\heroList.json", "r", encoding="utf8") as f:
         heroJsonStr = json.loads(f.read())
 
-        if 'hero' not in heroJsonStr.keys():  # 这一行的作用是判断读出来的文件内容对不对,如果是正确的内容则在json中有一个key值为hero.
-            return
+        cursor.execute("SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='heroinformation';")
+        if cursor.fetchall() == ():
+            create_database()  # 创建hero数据库
+            cursor.execute("use heroinformation;")
 
-        heroList = heroJsonStr['hero']  # 获取读取的json数据的hero键的值,应该是一个长度为150的json对象列表
-        keys = list(heroList[0].keys())  # 获取hero列表每一个对象的所有key,由于每个对象的key都相同,所以直接取下标为0的hero的key.
+            if 'hero' not in heroJsonStr.keys():  # 这一行的作用是判断读出来的文件内容对不对,如果是正确的内容则在json中有一个key值为hero.
+                print("Hero is not in heroJsonStr")
+                return
+            heroList = heroJsonStr['hero']  # 获取读取的json数据的hero键的值,应该是一个长度为150的json对象列表
+            keys = list(heroList[0].keys())  # 获取hero列表每一个对象的所有key,由于每个对象的key都相同,所以直接取下标为0的hero的key.
 
-        create_database()  # 创建hero数据库
-
-        if not table_isExists():  # 如果数据表不存在
             create_table(keys)  # 根据文件中读取出来的hero对象的key值列表创建数据表.
             insert_data('hero', heroList, keys)
+
+            other_keys = ['version', 'fileName', 'fileTime']
+            for key in other_keys:
+                create_table(None, key)
+            insert_other_data(heroJsonStr, other_keys)
+        else:
+            cursor.execute("use heroinformation;")
 
 
 if __name__ == '__main__':
